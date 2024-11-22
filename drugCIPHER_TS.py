fe6afb2d2f5d0c5c12f3525fbfa9913177e2a7dd
Huaishuo Liu <hs-liu21@mails.tsinghua.edu.cn>
2024-11-22 18:25:35 +0800

    update ts method

--- drugCIPHER_TS.py
+++ drugCIPHER_TS.py
@@ -1,34 +1,83 @@
 import pandas as pd
+from collections import Counter
+import json
 import numpy as np
-from utils import drugTS, target_to_drugs_interaction
+import networkx as nx
+import tqdm
+import random
+from sklearn.model_selection import train_test_split
+from utils import cal_test_ts, cal_test_closeness, shortest_path_length_dict
 
+# df_atc = df[['dg_id', 'dg_name','dg_atc_codes','dg_atc_levels']]
+df_atc = pd.read_csv("data_atc.csv")
+# for rows with same dg_id, keep only one row
+df_atc = df_atc.drop_duplicates(subset="dg_id")
+df_atc = df_atc[df_atc["dg_atc_codes"].apply(lambda x: x != "[]")].reset_index(
+    drop=True
+)
+df_atc["dg_atc_codes"] = df_atc["dg_atc_codes"].apply(lambda x: eval(x))
+df_atc["dg_atc_levels"] = df_atc["dg_atc_levels"].apply(lambda x: eval(x))
 
-class drug_target_score_TS:
-    """
-    output a score that represents the interaction between the drug targets and the target protein
-    """
+# train test split for df_atc by 8:2
+train, test = train_test_split(df_atc, test_size=0.2, random_state=42)
 
-    data_atc = pd.read_csv("data_atc.csv")
+# print the length of train and test
+print(f"Train length: {len(train)}")
+print(f"Test length: {len(test)}")
 
-    def __init__(self, drug, target):
-        self.drug = drug
-        self.target = target
-        self._convert_drugid()
+print("**************************")
+print("|       Drug CIPHER      |")
+print("**************************")
 
-    def _convert_drugid(self):
-        if self.drug in self.data_atc.dg_id.values:
-            self.drug_atc = self.data_atc[
-                self.data_atc.dg_id == self.drug
-            ].dg_atc_codes.values[0]
-            self.drug_level = self.data_atc[
-                self.data_atc.dg_id == self.drug
-            ].dg_atc_levels.values[0]
+flatten_train = [j for i in train["dg_atc_codes"] for j in i] + [
+    k for i in train["dg_atc_levels"] for j in i for k in j
+]
+counts_dict = dict(Counter(flatten_train))
+ppi_net = nx.read_graphml("ppi_network.graphml")
+targets = list(ppi_net.nodes())
 
-    def score(self):
-        drugTS_list = drugTS(self.drug_atc, self.drug_level)
-        target_drug_list = target_to_drugs_interaction(self.target)
-        cov = np.cov(drugTS_list, target_drug_list)
-        std_drug = np.std(drugTS_list)
-        std_target = np.std(target_drug_list)
-        score = cov / (std_drug * std_target)
-        return score
+drug_ts_test = cal_test_ts(train, test, counts_dict)
+
+ensp_count = json.load(open("target_ENSP_count.json"))
+ensp = sorted(ensp_count, key=lambda x: ensp_count[x], reverse=True)
+# target = "ENSP00000261707"
+
+# choose the top 10 targets
+for i in range(1):
+    target = ensp[i]
+    target_closeness = cal_test_closeness(train, target, ppi_net)
+    # print("target_closeness: ", target_closeness)
+    score_list = []
+    # old = None
+    for j in range(len(test)):
+        drugTS_list = drug_ts_test[i]
+        # if old == drugTS_list:
+        #     print("The same")
+        #     print("*****************")
+        # else:
+        #     print("Different")
+        #     print("*****************")
+        old = drugTS_list
+        print("drugTS_list: ", drugTS_list)
+        if np.std(drugTS_list) == 0 or np.std(target_closeness) == 0:
+            score = 0
+        else:
+            score = np.corrcoef(drugTS_list, target_closeness)[0, 1]
+            score = abs(score)
+
+        score_list.append((score, j))
+    score_list_sorted = sorted(score_list, key=lambda x: x[0], reverse=True)
+    # print the top 10 drugs
+    print("Target: ", target)
+    for i in range(10):
+        print("=====================================")
+        print(f"Drug: {test['dg_name'].iloc[score_list_sorted[i][1]]}")
+        print(f"Score: {score}")
+
+    # save the log
+    with open("log.txt", "a") as f:
+        f.write(f"Target: {target}\n")
+        for i in range(10):
+            f.write("=====================================\n")
+            f.write(f"Drug: {test['dg_name'].iloc[score_list_sorted[i][1]]}\n")
+            f.write(f"Score: {score}\n")
--- utils.py
+++ utils.py
@@ -2,38 +2,63 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 import json
+import os
+import tqdm
 from collections import Counter
 import math
+
+if os.path.exists("shortest_path_length_dict.json"):
+    shortest_path_length_dict = json.load(open("shortest_path_length_dict.json"))
+else:
+    shortest_path_length_dict = {}
+
+if os.path.exists("data_target_ENSP.json"):
+    drug_targets = json.load(open("data_target_ENSP.json"))
+else:
+    raise FileNotFoundError("data_target_ENSP.json not found")
+
 
 def shortest_path_length(ppi_network, ensp1, ensp2):
     """
     Given ppi_network, ensp1, ensp2, return the shortest path length between ensp1 and ensp2 in the network.
     """
     try:
-        length = nx.shortest_path_length(
-            ppi_network, source=ensp1, target=ensp2, weight="weight"
-        )
+        check1 = "(" + ensp1 + "," + ensp2 + ")"
+        check2 = "(" + ensp2 + "," + ensp1 + ")"
+        if check1 in shortest_path_length_dict:
+            length = shortest_path_length_dict[check1]
+        elif check2 in shortest_path_length_dict:
+            length = shortest_path_length_dict[check2]
+        else:
+            length = nx.shortest_path_length(
+                ppi_network, source=ensp1, target=ensp2, weight="weight"
+            )
+            shortest_path_length_dict[check1] = length
     except nx.NetworkXNoPath:
+        length = -1
+    except nx.NodeNotFound:
+        # If the node is not in the network, return -1
         length = -1
     return length
 
 
-def drug_target_interaction(drug_targets, target):
+def drug_target_interaction(ppi, drug_targets, target):
     """
     drug_targets: list of proteins that are targets of a drug
     target: protein
     return: a score that represents the interaction between the drug targets and the target protein
     """
-    ppi = nx.read_graphml("ppi_network.graphml")
 
     res = 0
     for p in drug_targets:
         length = shortest_path_length(ppi, p, target)
         if length != -1:
+            length = length / 1000
             res += np.exp(-(length**2))
     return res
 
-def cal_code_ts(code1, code2, level1, level2):
+
+def cal_code_ts(counts_dict, code1, code2, level1, level2):
     # find the longest common element of the levels
     common_level = set(level1).intersection(set(level2))
     if common_level:
@@ -43,7 +68,9 @@ def cal_code_ts(code1, code2, level1, level2):
     p_c1 = 0 if code1 not in counts_dict else counts_dict[code1]
     p_c2 = 0 if code2 not in counts_dict else counts_dict[code2]
     if p_c1 == 0 or p_c2 == 0 or p_mica == 0:
-        return 0  # If any probability is 0, return similarity as 0 (no shared information)
+        return (
+            0  # If any probability is 0, return similarity as 0 (no shared information)
+        )
 
     # Compute similarity using the provided formula
     similarity = (2 * math.log(p_mica + 1e-6)) / (
@@ -51,21 +78,51 @@ def cal_code_ts(code1, code2, level1, level2):
     )
     return similarity
 
-def cal_ts(code_l1, code_l2, level_l1, level_l2):
+
+def cal_ts(counts_dict, code_l1, code_l2, level_l1, level_l2):
     ts = 0
     for idx1, code1 in enumerate(code_l1):
         for idx2, code2 in enumerate(code_l2):
-            ts = max(cal_code_ts(code1, code2, level_l1[idx1], level_l2[idx2]), ts)
+            ts = max(
+                cal_code_ts(counts_dict, code1, code2, level_l1[idx1], level_l2[idx2]),
+                ts,
+            )
     return ts
 
-def cal_test_ts(train, test):
-    return [
-        [cal_ts(train["dg_atc_codes"].iloc[i], test["dg_atc_codes"].iloc[j], train["dg_atc_levels"].iloc[i], test["dg_atc_levels"].iloc[j]) for i in range(len(train))]
-    for j in range(len(test))]
 
-
-def target_to_drugs_interaction(target):
-    drug_targets = json.load(open("drug_target_ENSP.json"))
+def cal_test_ts(train, test, counts_dict):
     return [
-        drug_target_interaction(drug_targets[drug], target) for drug in drug_targets
+        [
+            cal_ts(
+                counts_dict,
+                train["dg_atc_codes"].iloc[i],
+                test["dg_atc_codes"].iloc[j],
+                train["dg_atc_levels"].iloc[i],
+                test["dg_atc_levels"].iloc[j],
+            )
+            for i in range(len(train))
+        ]
+        for j in range(len(test))
     ]
+
+
+def target_to_drugs_interaction(ppi, drug, target):
+    targets = None
+    for d in drug_targets:
+        if d["dg_id"] == drug:
+            targets = d["target_ENSP"]
+            break
+    if targets is None:
+        raise ValueError(f"Drug {drug} not found in drug_targets")
+    return drug_target_interaction(ppi, targets, target)
+
+
+def cal_test_closeness(train, target, ppi):
+    print(f"Calculating closeness for target {target}")
+    res = [
+        target_to_drugs_interaction(ppi, train["dg_id"].iloc[i], target)
+        for i in tqdm.tqdm(range(len(train)))
+    ]
+    with open("shortest_path_length_dict.json", "w") as f:
+        json.dump(shortest_path_length_dict, f, indent=2)
+    return res
